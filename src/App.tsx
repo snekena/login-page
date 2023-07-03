5197b3e3545aef8b84b7ff4ddd9f29a89361c76a
snekena <safnekena1@gmail.com>
2023-07-03 19:19:22 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
+import './assets/global.css';
+
+function App() {
+  return (
+    <>
+      <div className='app'>app</div>
+    </>
+  );
+}
+
+export default App;
